16a6909cf9a8c2ef11a980bd2e6df14070356129
innocentree <innocent.tree@gmail.com>
2019-09-16 23:47:19 +0900

    github 리모트 연결하는 법 주석으로 남김

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,14 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-
+/*
+ * github remote 연결하기
+ * 1. github 에 repository 만든다
+ * 2. vscode 터미널에서 git remode add origin 주소 입력
+ * 3. vscode 좌측 하단 master 라고 적힌 오른쪽에 클라우드 마크 누름
+ * 4. github 로그인창 나오면 로그인
+ * 5. 로컬에 commit 된 내용들이 push 된다.
+*/
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
 import 'FavoriteWords.dart';
