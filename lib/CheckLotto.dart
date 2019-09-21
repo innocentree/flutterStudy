ca3e3a4d5512ecd5ea3fd4865a872e08938a94bb
innocentree <innocent.tree@gmail.com>
2019-09-21 12:55:21 +0900

    위젯을 빌드에서 새로 만들지 않고 밖에서 만들면 데이터가 갱신되지 않음.

--- lib/CheckLotto.dart
+++ lib/CheckLotto.dart
@@ -13,17 +13,21 @@ class CheckLottoState extends State<CheckLotto> {
   final winnerCount = 3;
   final winnerPrice = 5;
 
-  final lottoUrl ='https://www.dhlottery.co.kr/gameResult.do?method=byWin'; 
+  final lottoUrl = 'https://www.dhlottery.co.kr/gameResult.do?method=byWin';
   String lastWinNumber = "";
   var widgetWinNumber = Text("wait for Data...");
   var widgetWinPrice = Text("");
-  List<String> DataPolishing(List<String> inData){
+  var lottoStageList = <String>[];
+  
+  String _dropdownValue = '-';
+
+  List<String> DataPolishing(List<String> inData) {
     var foundData = <String>[];
     for (int i = 0; i < inData.length; i++) {
       //if (inData[i].split('.').first) {
-        if (inData[i].contains(new RegExp(r'[0-9]'))){
-          var e = inData[i].replaceAll(new RegExp(r'[^0-9^+^,]'), '');
-          foundData.add(e);
+      if (inData[i].contains(new RegExp(r'[0-9]'))) {
+        var e = inData[i].replaceAll(new RegExp(r'[^0-9^+^,]'), '');
+        foundData.add(e);
       }
     }
     return foundData;
@@ -34,6 +38,11 @@ class CheckLottoState extends State<CheckLotto> {
     data.then((value) {
       final splited = value.split(' ');
       final numberList = DataPolishing(splited);
+      _dropdownValue = numberList[stageNum];
+      lottoStageList.clear();
+      for (int i = 1; i <= int.parse(numberList[stageNum]); i++) {
+        lottoStageList.insert(0, i.toString());
+      }
       lastWinNumber = numberList[winnerLottoNum];
       setState(() {
         widgetWinNumber = Text("당첨번호 : " + numberList[winnerLottoNum]);
@@ -51,19 +60,41 @@ class CheckLottoState extends State<CheckLotto> {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child:Column(
-        children:<Widget>[
-          widgetWinNumber,
-          widgetWinPrice,
-          FlatButton(
-            child: Text("데이터 갱신"),
-            onPressed: () => RefreshData(),
-            color: Color.fromRGBO(100, 100, 100, 200),
-            splashColor: Colors.cyan,
-            focusColor: Colors.indigo,
-          ),
-        ],
-        )
-        );
+        child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        widgetWinNumber,
+        widgetWinPrice,
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            DropdownButton<String>(
+              onChanged: (String newValue) {
+                setState(() {
+                  _dropdownValue = newValue;
+                  print("_dropdownValue : " + _dropdownValue);
+                  print("newValue : " + newValue);
+                });
+              },
+              value: _dropdownValue,
+              items:
+                  lottoStageList.map<DropdownMenuItem<String>>((String value) {
+                return DropdownMenuItem<String>(
+                  value: value,
+                  child: Text(value),
+                );
+              }).toList(),
+            ),
+            FlatButton(
+              child: Text("데이터 갱신"),
+              onPressed: () => RefreshData(),
+              color: Color.fromRGBO(100, 100, 100, 200),
+              splashColor: Colors.cyan,
+              focusColor: Colors.indigo,
+            ),
+          ],
+        ),
+      ],
+    ));
   }
 }
